a7b8b6c94701d194dedf73754fcff37b8a8d975f
tms <tomas.rls@gmail.com>
2023-02-08 11:16:32 +0200

    changed redirect url for login

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -132,6 +132,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/mytasks/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
